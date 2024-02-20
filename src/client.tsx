51c897b3062e8ff02fc9ac8a0693d008a2522b85
melonMochiRoll <soulhwasin@gmail.com>
2024-02-20 17:31:32 +0900

    :sparkles: client 파일 추가

---
+++ src/client.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const rootNode = document.getElementById('root') as HTMLElement;
+
+createRoot(rootNode).render(
+  <React.StrictMode>
+    
+  </React.StrictMode>
+);

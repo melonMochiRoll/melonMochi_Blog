5a7b39e2d06eb15d743eccdc636c875da64b67e1
melonMochiRoll <soulhwasin@gmail.com>
2024-02-20 18:02:29 +0900

    :heavy_plus_sign: router 불러오기

--- src/client.tsx
+++ src/client.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 const rootNode = document.getElementById('root') as HTMLElement;
 
 createRoot(rootNode).render(
   <React.StrictMode>
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

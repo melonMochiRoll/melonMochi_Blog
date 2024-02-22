02ffd1cca2295ded72c88c4ab16ef6f0e5c6f5c4
melonMochiRoll <soulhwasin@gmail.com>
2024-02-22 19:49:43 +0900

    :lipstick: Apply GlobalStyle

--- src/client.tsx
+++ src/client.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { GlobalStyle } from './GlobalStyle';
 
 const rootNode = document.getElementById('root') as HTMLElement;
 
 createRoot(rootNode).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );

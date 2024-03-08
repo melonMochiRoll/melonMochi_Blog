8b3b84d56feb800167d2d2e004a5c9d483cd6a8a
melonMochiRoll <soulhwasin@gmail.com>
2024-03-08 19:45:30 +0900

    :wrench: components 폴더 경로 별칭 추가

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
+      '@Components': path.resolve(__dirname, 'src/components/'),
       '@Containers': path.resolve(__dirname, 'src/containers/'),
       '@Pages': path.resolve(__dirname, 'src/pages/'),
     },

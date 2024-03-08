c7071a826be9b8500640be5be3e9edc66d122103
melonMochiRoll <soulhwasin@gmail.com>
2024-03-08 20:10:00 +0900

    :truck: MainPage로 이름 롤백

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import Main from '@Containers/Main';
+import PageTemplate from '@Components/PageTemplate';
+
+const MainPage: FC = () => {
+  return (
+    <PageTemplate>
+      <Main />
+    </PageTemplate>
+  );
+};
+
+export default MainPage;

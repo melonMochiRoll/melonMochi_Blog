0bf07758fc05710270482b0fb02706839e1aa5c0
melonMochiRoll <soulhwasin@gmail.com>
2024-03-18 17:18:23 +0900

    :sparkles: Assets 모듈 타입 선언

---
+++ src/assets.d.ts
@@ -0,0 +1,14 @@
+
+// fonts
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.eot";
+declare module "*.ttf";
+declare module "*.otf";
+
+// images
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";

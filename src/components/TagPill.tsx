71952ad22552b63a4f7aaca2891b3fe7aad618a2
melonMochiRoll <soulhwasin@gmail.com>
2024-03-19 17:51:15 +0900

    :sparkles: TagPill 컴포넌트 추가

---
+++ src/components/TagPill.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+
+interface TagPillProps {
+  tagName: string;
+};
+
+const TagPill: FC<TagPillProps> = ({
+  tagName,
+}) => {
+  return (
+    <Block>
+      {tagName}
+    </Block>
+  );
+};
+
+export default TagPill;
+
+const Block = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 600;
+  border: 2px solid #01a020;
+  border-radius: 25px;
+  padding: 8px 15px;
+  background-color: #01a020;
+  gap: 4px;
+
+  &:hover {
+    border: 2px solid #01a020;
+  }
+`;

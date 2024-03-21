3db781fb81cdecd64bc43b326f1127064124c051
melonMochiRoll <soulhwasin@gmail.com>
2024-03-21 21:23:00 +0900

    :sparkles: PostPage 라우터 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '@Pages/MainPage';
+import PostPage from '@Pages/PostPage';
 
 const MainRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainPage />,
-  },
+    {
+      path: '/',
+      element: <MainPage />,
+    },
+    {
+      path: 'post/:fileName',
+      element: <PostPage />,
+    },
 ]);
 
 export default MainRouter;

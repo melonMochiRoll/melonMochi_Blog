025e288d5becaa5cd4e6cde8eee18c58d52241f8
melonMochiRoll <soulhwasin@gmail.com>
2024-03-25 16:01:51 +0900

    :lipstick: 글 목록 태그 추가

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,35 +1,42 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+import { ETags, TPostInfo } from '@Typings/post';
 
-const Main: FC = () => { // RecentPosts 혹은 선택된 태그에 해당하는 게시글 출력
+interface MainProps {
+  list: any;
+};
+
+const Main: FC<MainProps> = ({
+  list,
+}) => {
+  const navigate = useNavigate();
+
   return (
-    <Block>
-      <Tags>
-        TAGS
-      </Tags>
-      <RecentPosts>
-        <Article>
-          <Title>Title</Title>
-          <Paragraph>
-            Lorem Ipsum...
-          </Paragraph>
-          <Timestamp>2024-02-22</Timestamp>
-        </Article>
-        <Article>
-          <Title>Title</Title>
-          <Paragraph>
-            Lorem Ipsum...
-          </Paragraph>
-          <Timestamp>2024-02-22</Timestamp>
-        </Article>
-      </RecentPosts>
-    </Block>
+    <Posts>
+      {
+        list.map((post: TPostInfo, idx: number) => 
+          <Article
+            key={idx}>
+            <TagInfo>
+              {post.tags?.map((tag: ETags, idx: number) =>
+                <TagDisplay key={idx}>{tag}</TagDisplay>
+              )}
+            </TagInfo>
+            <Info onClick={() => navigate(`post/${post.id}`)}>
+              <Title>{post.title}</Title>
+              <Paragraph>{post.preview}</Paragraph>
+              <CreatedAt>{post.createdAt}</CreatedAt>
+            </Info>
+          </Article>)
+      }
+    </Posts>
   );
 };
 
 export default Main;
 
-const Block = styled.main`
+const Posts = styled.main`
   width: 700px;
   min-height: 100vh;
   display: flex;
@@ -41,43 +48,68 @@ const Block = styled.main`
   }
 `;
 
-const Tags = styled.section`
-  width: 100%;
-  height: 200px;
-  display: flex;
-  align-items: center;
-  padding: 20px 50px;
-`;
-
-const RecentPosts = styled.section`
-  width: 100%;
-  min-height: 80vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 const Article = styled.article`
   width: 100%;
   display: flex;
   flex-direction: column;
-  padding: 20px;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 20px 20px;
   border-bottom: 1px solid #e9ecef;
+  gap: 15px;
+`;
+
+const TagInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const TagDisplay = styled.div`
+  display: flex;
+  align-items: center;
+  color: #0d47a1;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 25px;
+  padding: 8px 15px;
+  background-color: #d2f7ff;
+  gap: 4px;
+  cursor: pointer;
+  transition: all 0.1s linear;
+
+  &:hover {
+    color: #fff;
+    background-color: #0d47a1;
+  }
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 15px;
+
+  &:hover {
+    color: #29629C;
+  }
 `;
 
 const Title = styled.h1`
   font-size: 32px;
   margin: 0;
-  margin-bottom: 10px;
+  cursor: pointer;
 `;
 
 const Paragraph = styled.p`
   display: flex;
   flex-direction: column;
+  font-size: 18px;
   margin: 0;
-  margin-bottom: 10px;
 `;
 
-const Timestamp = styled.em`
+const CreatedAt = styled.span`
+  font-size: 16px;
   color: gray;
 `;

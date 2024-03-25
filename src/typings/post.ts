06e130480a8127a0a963fedafd3832f370a08757
melonMochiRoll <soulhwasin@gmail.com>
2024-03-25 16:06:09 +0900

    :lipstick: Etags enum 추가

--- src/typings/post.ts
+++ src/typings/post.ts
@@ -1,7 +1,13 @@
+
+export const enum ETags {
+  Javascript = 'Javascript',
+  ETC = 'etc',
+};
+
 export type TPostInfo = {
   id: number,
   title: string,
-  tags: string[],
+  tags: ETags[],
   preview: string,
   createdAt: string,
 };

32053d797344859809c90d9df596c6fbf576d235
melonMochiRoll <soulhwasin@gmail.com>
2024-03-26 18:10:05 +0900

    :wrench: Posts 경로 별칭 추가

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
       '@Components': path.resolve(__dirname, 'src/components/'),
       '@Containers': path.resolve(__dirname, 'src/containers/'),
       '@Pages': path.resolve(__dirname, 'src/pages/'),
+      '@Posts': path.resolve(__dirname, 'src/posts/'),
       '@Typings': path.resolve(__dirname, 'src/typings/'),
     },
   },

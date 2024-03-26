57f97bb0d08dec61779b5f1c67ab53329753be47
melonMochiRoll <soulhwasin@gmail.com>
2024-03-26 18:38:56 +0900

    :art: 태그 일부 Article Components로 분리

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 import { TPostInfo } from '@Typings/post';
+import Article from '@Components/Article';
 
 interface MainProps {
   posts: TPostInfo[];
@@ -17,15 +18,9 @@ const Main: FC<MainProps> = ({
       {
         posts.map((post: TPostInfo, idx: number) => 
           <Article
-            key={idx}>
-            <TagInfo>
-              {post.tags?.map((tag: string, idx: number) =>
-                <TagDisplay key={idx}>{tag}</TagDisplay>
-              )}
-            </TagInfo>
-            <Title onClick={() => navigate(`posts/${post.fileName}`)}>{post.title}</Title>
-            <CreatedAt>{post.createdAt}</CreatedAt>
-          </Article>)
+            key={idx}
+            post={post}
+            navigate={() => navigate(`posts/${post.fileName}`)}/>)
       }
     </Posts>
   );
@@ -45,53 +40,6 @@ const Posts = styled.main`
   }
 `;
 
-const Article = styled.article`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  padding: 20px 20px;
-  border-bottom: 1px solid #e9ecef;
-  gap: 15px;
-`;
+const LoadMore = styled.button`
 
-const TagInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-`;
-
-const TagDisplay = styled.div`
-  display: flex;
-  align-items: center;
-  color: #0d47a1;
-  font-size: 14px;
-  font-weight: 600;
-  border-radius: 25px;
-  padding: 8px 15px;
-  background-color: #d2f7ff;
-  gap: 4px;
-  cursor: pointer;
-  transition: all 0.1s linear;
-
-  &:hover {
-    color: #fff;
-    background-color: #0d47a1;
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 36px;
-  margin: 0;
-  cursor: pointer;
-
-  &:hover {
-    color: #29629C;
-  }
-`;
-
-const CreatedAt = styled.span`
-  font-size: 16px;
-  color: gray;
 `;

5cc77759021dcc2f4001d07dfdc73804ecd8df6b
melonMochiRoll <soulhwasin@gmail.com>
2024-03-26 17:58:02 +0900

    :sparkles: PostPage 추가

---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import PostHeader from '@Containers/PostHeader';
+import PostArticle from '@Containers/PostArticle';
+import { useParams } from 'react-router-dom';
+
+const PostPage: FC = () => {
+  const { fileName } = useParams();
+  const { info, content } = require(`@Assets/posts/${fileName}`);
+  const [ post, setPost ] = useState('');
+
+  useEffect(() => {
+    fetch(content)
+      .then(res => res.text())
+      .then(str => setPost(str));
+  }, []);
+
+  return (
+    <Block>
+      <PostHeader
+        info={info} />
+      <PostArticle
+        post={post} />
+    </Block>
+  );
+};
+
+export default PostPage;
+
+const Block = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;

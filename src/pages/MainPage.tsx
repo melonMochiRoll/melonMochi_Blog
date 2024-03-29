3c0f9798df755cd4fe4ebf6f79670e6ddb81b73c
melonMochiRoll <soulhwasin@gmail.com>
2024-03-29 21:48:37 +0900

    :art: 최근 글 목록 출력

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,33 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import Main from '@Containers/Main';
-import ListHeader from '@Containers/ListHeader';
+import Header from '@Containers/Header';
+import { getPosts } from '@Posts/index';
 
 const MainPage: FC = () => {
+  const [ posts, setPosts ] = useState(getPosts(0, 6));
+  const [ canLoadMore, setCanLoadMore ] = useState(true);
+  const loadCount = useRef(1);
+  
+  const getMorePosts = () => {
+    const cursor = loadCount.current * 6;
+    const morePosts = getPosts(0 + cursor, 6 + cursor);
+
+    if (morePosts.length < 6) {
+      setCanLoadMore(false);
+    }
+
+    setPosts((prev) => [...prev, ...morePosts]);
+    ++loadCount.current;
+  };
+  
   return (
     <Block>
-      <ListHeader />
-      <Main />
+      <Header />
+      <Main
+        posts={posts}
+        getMorePosts={getMorePosts}
+        canLoadMore={canLoadMore} />
     </Block>
   );
 };

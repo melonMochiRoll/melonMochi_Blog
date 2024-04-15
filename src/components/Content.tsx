fdd7aab0adf9bacfc5ffaf2ae9ca9b5155ce1acb
melonMochiRoll <soulhwasin@gmail.com>
2024-04-15 23:30:03 +0900

    :lipstick: Article css 수정

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -28,21 +28,30 @@ export default Content;
 const Article = styled.article`
   width: 700px;
   min-height: 100vh;
+  padding-bottom: 30vh;
 
   h1 {
     font-size: 48px;
+    margin-top: 50px;
   }
 
   h2 {
     font-size: 36px;
+    margin-top: 50px;
   }
 
   p {
     font-size: 20px;
+    line-height: 2em;
+  }
+
+  pre {
+    margin: 50px 0;
   }
   
   hr {
     border: 1px solid #E3E3E3;
+    margin-bottom: 50px;
   }
 
   @media only screen and (max-width: 768px) {

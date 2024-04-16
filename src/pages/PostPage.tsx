8726140122f6c248551bdafa77c2a15b7c17118b
melonMochiRoll <soulhwasin@gmail.com>
2024-04-16 23:05:17 +0900

    :art: 글 하단부 추가

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -4,13 +4,17 @@ import PostHeader from '@Containers/PostHeader';
 import Content from '@Components/Content';
 import { useNavigate, useParams } from 'react-router-dom';
 import Footer from '@Containers/Footer';
-import { getPost } from '@Posts/index';
+import { getPost, getPosts } from '@Posts/index';
+import { TPostInfo } from '@Typings/post';
+import ArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeftRounded';
+import ArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRightRounded';
 
 const PostPage: FC = () => {
   const navigate = useNavigate();
   const { fileName } = useParams();
   const result = getPost(Number(fileName));
   const [ post, setPost ] = useState('');
+  const [ recentPosts ] = useState(getPosts(undefined, 2, fileName));
 
   useEffect(() => {
     if (!result) {
@@ -30,6 +34,28 @@ const PostPage: FC = () => {
         info={result.info} />
       <Content
         post={post} />
+      <Bottom>
+        <Back
+          onClick={() => navigate(-1)}>
+          <ArrowLeftIcon fontSize='large' />
+          돌아가기
+        </Back>
+        <RecentPostsDiv>
+        <span>최근 글</span>
+        {
+          recentPosts?.posts.map((ele: TPostInfo, idx: number) => {
+            return (
+              <Back
+                key={idx}
+                onClick={() => navigate(`/post/${ele.fileName}`)}>
+                {ele.title}
+                <ArrowRightIcon fontSize='large' />
+              </Back>
+            );
+          })
+        }
+        </RecentPostsDiv>
+      </Bottom>
       <Footer />
     </Block>
   );
@@ -42,4 +68,39 @@ const Block = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 750px;
+  border-top: 1px solid #00af22;
+  padding-bottom: 100px;
+`;
+
+const Back = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 600;
+  gap: 5px;
+  cursor: pointer;
+
+  &:hover {
+    color: #29629C;
+  }
+`;
+
+const RecentPostsDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+  gap: 5px;
+
+  span {
+    font-size: 20px;
+    font-weight: 600;
+  }
 `;

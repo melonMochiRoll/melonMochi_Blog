43cf01b3c07d64f238bdcdb5c530e44c458f41a0
melonMochiRoll <soulhwasin@gmail.com>
2024-05-07 20:36:45 +0900

    :lipstick: metaData 출력 수정

--- app/post/[tag]/[fileName]/page.tsx
+++ app/post/[tag]/[fileName]/page.tsx
@@ -1,6 +1,11 @@
-import CustomMDX from "@/App/post/[tag]/[fileName]/_components/CustomMDX";
+import fs from 'fs';
+import path from "path";
+import matter from 'gray-matter';
 import { Suspense } from "react";
+import CustomMDX from "@/App/post/[tag]/[fileName]/_components/CustomMDX";
 import Loading from "@/App/post/[tag]/[fileName]/loading";
+import { metadata } from '@/App/layout';
+import FixedBox from './_components/FixedBox';
 
 type TPostPageProps = {
   params: {
@@ -9,16 +14,32 @@ type TPostPageProps = {
   },
 };
 
+export async function generateMetadata({
+  params,
+}: TPostPageProps) {
+  const { tag, fileName } = params;
+  const filePath = path.join(process.cwd(), 'posts', tag, `${fileName}.mdx`);
+  const mdxSource = fs.readFileSync(filePath, 'utf-8');
+  const { data } = matter(mdxSource);
+
+  return {
+    title: `${data.title || ''} | ${metadata.title}`,
+  };
+}
+
 export default function PostPage({
   params,
 }: TPostPageProps) {
   const { tag, fileName } = params;
 
   return (
-    <Suspense fallback={<Loading />}>
-      <CustomMDX
-        tag={tag}
-        fileName={fileName} />
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading />}>
+        <CustomMDX
+          tag={tag}
+          fileName={fileName} />
+      </Suspense>
+      <FixedBox />
+    </>
   );
 }
--- app/posts/[tag]/page.tsx
+++ app/posts/[tag]/page.tsx
@@ -1,9 +1,18 @@
 import Header from "@/App/_components/Header";
+import { metadata } from "@/App/layout";
 import PostsByTag from "@/App/posts/[tag]/_components/PostsByTag";
 
 type TPostsPageProps = {
   params: { tag: string },
 };
+
+export async function generateMetadata({
+  params,
+}: TPostsPageProps) {
+  return {
+    title: `${params.tag} | ${metadata.title}`,
+  };
+}
 
 export default async function PostsPage({
   params,

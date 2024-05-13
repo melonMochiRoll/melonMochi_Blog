f992825df8760c904ae2e5247f0425af66a8a65f
melonMochiRoll <soulhwasin@gmail.com>
2024-05-13 22:44:26 +0900

    :sparkles: favicon 추가

---
+++ app/icon.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from 'next/og'
+ 
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+ 
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 26,
+          fontWeight: 800,
+          background: '#16BE4B',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: '#fff',
+        }}
+      >
+        M
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}

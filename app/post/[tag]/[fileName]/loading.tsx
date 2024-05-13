ef480a4548982781641ec437cb4fe1d58453a0f7
melonMochiRoll <soulhwasin@gmail.com>
2024-05-13 20:42:38 +0900

    :lipstick: 로딩화면 더 직관적으로 수정

--- app/post/[tag]/[fileName]/loading.tsx
+++ app/post/[tag]/[fileName]/loading.tsx
@@ -1,11 +1,17 @@
-import PostHeader from '@/App/post/[tag]/[fileName]/_components/PostHeader';
-import Content from '@/App/post/[tag]/[fileName]/_components/Content';
+import styles from '@/App/post/[tag]/[fileName]/_components/styles/PostHeader.module.css';
+import { CircularProgress } from '@mui/material';
 
 export default function Loading() {
   return (
     <>
-      <PostHeader />
-      <Content />
+      <header className={styles.header}>
+        <div className={styles.cover}>
+          <div className={styles.postInfo}>
+            <CircularProgress size={150} />
+          </div>
+        </div>
+      </header>
+      <main className={styles.skeleton} />
     </>
   );
 }

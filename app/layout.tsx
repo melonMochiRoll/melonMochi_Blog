b47859d51a8813e7cf09b499670127af3fee4267
melonMochiRoll <soulhwasin@gmail.com>
2024-05-16 19:06:33 +0900

    :chart_with_upwards_trend: 구글 애널리틱스 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,22 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <head>
         <meta name="google-site-verification" content={process.env.GOOGLE_SITE_VERIFICATION_CONTENT} />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {
+                dataLayer.push(arguments)
+              }
+              gtag('js', new Date());
+
+              gtag('config', ${process.env.GA_ID});
+            `
+          }}>
+        </script>
       </head>
       <body>
         <div className='main'>

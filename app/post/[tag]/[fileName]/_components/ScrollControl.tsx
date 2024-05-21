1d4fb82301ff4f23152d749d24bb7afdef97c3f5
melonMochiRoll <soulhwasin@gmail.com>
2024-05-22 00:52:23 +0900

    :truck: ScrollControl로 이름 변경

---
+++ app/post/[tag]/[fileName]/_components/ScrollControl.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import styles from './styles/ScrollControl.module.css';
+import HomeIcon from '@mui/icons-material/Home';
+import UpArrowIcon from '@mui/icons-material/NorthRounded';
+import { useRouter } from 'next/navigation';
+
+export default function ScrollControl() {
+  const router = useRouter();
+
+  const scrollToY = (y: number) => {
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth'
+    });
+  };
+
+  return (
+    <div className={styles.box}>
+      <div
+        onClick={() => scrollToY(0)}
+        className={styles.item}>
+        <UpArrowIcon fontSize='small' />
+      </div>
+      <div
+        onClick={() => router.push('/')}
+        className={styles.item}>
+        <HomeIcon fontSize='small' />
+      </div>
+    </div>
+  );
+}

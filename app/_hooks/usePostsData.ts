7a448c5f2b925243c122761a58dd8ba49844619c
melonMochiRoll <soulhwasin@gmail.com>
2024-06-13 21:10:13 +0900

    :art: 게시글 불러오는 로직 hooks로 분리

---
+++ app/_hooks/usePostsData.ts
@@ -0,0 +1,56 @@
+import { getPostsByTag } from "@/Lib/post";
+import { TPagination } from "@/Lib/typing";
+import { useEffect, useState } from "react";
+
+type TUsePostDataReturnType = {
+  pagination: TPagination,
+  isLoading: boolean,
+  loadMore: () => Promise<void>,
+  canLoadMore: boolean,
+};
+
+export default function usePostsData(tag: string): TUsePostDataReturnType {
+  const [ pagination, setPagination ] = useState<TPagination>({ cursor: 0, posts: [] });
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ canLoadMore, setCanLoadMore ] = useState(true);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    setIsLoading(true);
+    const result = await getPostsByTag(pagination.cursor, tag);
+
+    if (result.posts.length < 6) {
+      setCanLoadMore(false);
+    }
+
+    setPagination(result);
+    setIsLoading(false);
+  };
+
+  const loadMore = async () => {
+    setIsLoading(true);
+    const result = await getPostsByTag(pagination.cursor, tag);
+
+    if (result.posts.length < 6) {
+      setCanLoadMore(false);
+    }
+
+    setPagination((prev: TPagination) => {
+      return {
+        cursor: result.cursor,
+        posts: [ ...prev.posts, ...result.posts ],
+      };
+    });
+    setIsLoading(false);
+  };
+
+  return {
+    pagination,
+    isLoading,
+    loadMore,
+    canLoadMore,
+  };
+}
--- app/posts/[tag]/_components/PostsByTag.tsx
+++ app/posts/[tag]/_components/PostsByTag.tsx
@@ -3,14 +3,8 @@
 import Article from "@/App/_components/Article";
 import LoadingPosts from "@/App/_components/LoadingPosts";
 import styles from "@/App/_components/styles/RecentPosts.module.css";
-import { getPostsByTag } from "@/Lib/post";
+import usePostsData from "@/App/_hooks/usePostsData";
 import { TMetaData } from "@/Lib/typing";
-import { useEffect, useState } from "react";
-
-type TPagination = {
-  cursor: number,
-  posts: TMetaData[],
-};
 
 type TPostsByTagProps = {
   tag: string,
@@ -19,42 +13,12 @@ type TPostsByTagProps = {
 export default function PostsByTag({
   tag,
 }: TPostsByTagProps) {
-  const [ pagination, setPagination ] = useState<TPagination>({ cursor: 0, posts: [] });
-  const [ isLoading, setIsLoading ] = useState(true);
-  const [ canLoadMore, setCanLoadMore ] = useState(true);
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  const getPosts = async () => {
-    const result = await getPostsByTag(pagination.cursor, tag);
-    setIsLoading(false);
-
-    if (result.posts.length < 6) {
-      setCanLoadMore(false);
-    }
-
-    setPagination(result);
-  };
-
-  const loadMore = async () => {
-    setIsLoading(true);
-
-    const result = await getPostsByTag(pagination.cursor, tag);
-    setIsLoading(false);
-
-    if (result.posts.length < 6) {
-      setCanLoadMore(false);
-    }
-
-    setPagination((prev: TPagination) => {
-      return {
-        cursor: result.cursor,
-        posts: [ ...prev.posts, ...result.posts ],
-      };
-    });
-  };
+  const {
+    pagination,
+    isLoading,
+    loadMore,
+    canLoadMore,
+  } = usePostsData(tag);
   
   return (
     <main className={styles.main}>

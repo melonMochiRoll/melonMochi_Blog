11d9a04227254db7f4b95a6d80c1ef8d5d70520b
melonMochiRoll <soulhwasin@gmail.com>
2024-07-16 22:25:53 +0900

    :label: tags 타입 수정

--- app/post/[tag]/[fileName]/_components/TagList.tsx
+++ app/post/[tag]/[fileName]/_components/TagList.tsx
@@ -1,8 +1,9 @@
 import styles from '@/App/post/[tag]/[fileName]/_components/styles/TagList.module.css';
+import { TTags } from '@/Lib/typing';
 import Link from 'next/link';
 
 type TTagListProps = {
-  tags: string[],
+  tags: TTags[],
 };
 
 export default function TagList({
@@ -13,12 +14,12 @@ export default function TagList({
       <strong>TAG LIST</strong>
       <div className={styles.tagList}>
         {
-          tags.map((tag: string, idx: number) => {
+          tags.map((ele: TTags, idx: number) => {
             return (
               <Link
-                key={tag + idx}
-                href={`/posts/${tag}`}>
-                {tag}
+                key={ele?.tag + idx}
+                href={`/posts/${ele?.tag}`}>
+                {ele?.tag}
               </Link>
             );
           })

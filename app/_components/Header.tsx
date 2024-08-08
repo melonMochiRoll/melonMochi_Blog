5fa5d764bde657249dd7d34bf008bf07ffd0ef17
melonMochiRoll <soulhwasin@gmail.com>
2024-08-08 21:54:47 +0900

    :art: 헤더 이미지 최적화 및 구조 수정

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,6 +2,7 @@ import styles from '@/App/_components/styles/Header.module.css';
 import Tag from '@/App/_components/Tag';
 import { getTags } from '@/Lib/post';
 import { TTags } from '@/Lib/typing';
+import Image from 'next/image';
 
 type THeaderProps = {
   tag?: string,
@@ -11,18 +12,24 @@ export default async function Header({
   tag = '',
 }: THeaderProps) {
   const tags = await getTags();
+  const headerBgSrc = '/images/main_bg.jpg';
 
   return (
     <header className={styles.header}>
+      <Image
+        className={styles.bgWrap}
+        fill
+        alt='background'
+        src={headerBgSrc}
+        quality={80}
+        priority={true} />
       <div className={styles.cover}>
         <div className={styles.listInfo}>
-          <h1 className={styles.title}>
-            {tag || '최근 글'}
-          </h1>
-          <section className={styles.section}>
-            <h2 className={styles.h2}>
-              Tags
-            </h2>
+          <section className={styles.topSection}>
+            <h1 className={styles.h1}>{tag || '최근 글'}</h1>
+          </section>
+          <section className={styles.bottomSection}>
+            <h2 className={styles.h2}>Tags</h2>
             <section className={styles.tagsSection}>
               {
                 tags.map((ele: TTags, idx: number) => {
